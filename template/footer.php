c95c5d63bd85a78bf7b43807f6947aec41369c68
David Flores <31556253+mrdavis-dev@users.noreply.github.com>
2020-12-20 15:01:24 -0500

    up

---
+++ template/footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+      <!-- <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <h3>Beta</h3>
+            <p>dcon esta en fase beta por lo que se estara actualizando constantemente agregando más funciones facilitando una mejor gestión del tiempo.</p>
+          </div>
+          <div class="col-lg-3 mx-auto">
+            <h3>Contacto</h3>
+            <ul class="list-unstyled">
+              <li><a href="#">correo</a></li>
+              <li><a href="#">Services</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-4">
+             <h3>Subscribe</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, quibusdam!</p>
+            <form action="#" class="form-subscribe">
+              <input type="email" class="form-control mb-3" placeholder="Enter Email">
+              <input type="submit" class="btn btn-primary" value="Subscribe">
+            </form> -
+          </div>
+        </div> -->
+
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>
+               <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> 
+               Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | develop by <a href="https://mrdavis-dev.github.io" target="_blank" >mrdavis-dev</a>
+               <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0.  -->
+            </p>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
